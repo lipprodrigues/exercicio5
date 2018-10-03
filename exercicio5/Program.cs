ef9e99b3b97ef54974b9d151773e40a9c795157f
luiz felipe <luh.ffelipe@hotmail.com>
2018-10-03 17:17:47 -0300

    exercicio5
    5° exercicio da lista 1

---
+++ exercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+            while (x <= 10)
+            {
+                Console.WriteLine("2x" + x + "=" + (x + x));
+                x = x + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
